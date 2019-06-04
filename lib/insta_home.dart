c8b9f2f0db1ba863db0205c8b330e0ab53dd3707
Eric Harper <eric.harper@bison.howard.edu>
2019-06-04 12:41:21 -0500

    intial commit

---
+++ lib/insta_home.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+class Instahome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/insta_home.dart'
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram',
+      theme: ThemeData
+        primarySwatch: Colors.blue,
+  primaryColor: Colors.black
+  home: Instahome ()
